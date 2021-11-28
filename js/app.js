6a9b2fd447135c63d6a0fcb143ffbc3fa825f978
Blake Beckstrom <dabeck88@gmail.com>
2021-11-27 23:13:09 -0500

    added the map

---
+++ js/app.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken =
+	"pk.eyJ1IjoiYmxha2ViZWNrc3Ryb20iLCJhIjoiY2t3aW9rb29zMWFpbDJvcWo5cTNvejN5YSJ9.7Ih28TwCOPbgypr8Y1NY0A";
+var map = new mapboxgl.Map({
+	container: "map",
+	style: "mapbox://styles/mapbox/dark-v10",
+	center: [-79.931053, 32.776474],
+	zoom: 9,
+});
